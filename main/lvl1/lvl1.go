2c9401c8d49890110f218befc412d3cc54860e3b
rvauradkar <rvauradkar@gmail.com>
2020-07-29 15:38:57 -0500

    added lvl1

---
+++ main/lvl1/lvl1.go
@@ -0,0 +1,29 @@
+package lvl1
+
+import (
+	"time"
+
+	"github.com/rvauradkar1/fuse/mock/lvl1/lvl2"
+)
+
+type Il1 interface {
+	LM1(i int, s string) string
+	LM2(t time.Duration, f float32) string
+}
+
+type L1 struct {
+	s     string
+	S1    string
+	time  time.Duration
+	Time2 time.Duration
+	L2    lvl2.L2
+	PL2   *lvl2.L2
+}
+
+func (l L1) LM1(i int, f float32) string {
+	return "return from LM1"
+}
+
+func (l *L1) LM2(t time.Duration, f float32) string {
+	return "return from LM2"
+}
